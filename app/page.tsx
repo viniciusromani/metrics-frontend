91d70f637831f92b0eced1e074297e22e1424423
Vinicius Romani <vn.romani@gmail.com>
2024-08-23 10:36:04 -0300

    adding prettier (and config file), husky and lint-staged to run linter on every commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <Button className="m-2">
-        Click me
-      </Button>
+      <Button className="m-2">Click me</Button>
       <div>Hello World</div>
     </main>
   );
